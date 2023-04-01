b6be8c3297c964a6b23f9753c37d509373c33fb3
Guantong <i@guantong.dev>
2023-04-01 09:25:16 +0800

    Save & Load window state

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -25,6 +25,7 @@ fn main() {
             load_conversations,
             save_conversations
         ])
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
