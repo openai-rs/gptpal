c7ab021773e5d8cc3057fe607496e51a57805af8
Guantong <i@guantong.dev>
2023-04-04 17:28:43 +0800

    Opt prompts

--- src-tauri/src/prompt.rs
+++ src-tauri/src/prompt.rs
@@ -1,74 +1,43 @@
 use std::collections::HashMap;
 
 use crate::chat::*;
-use select::document::Document;
-use select::predicate::Name;
 
 const PROMPTS_FILE: &str = "prompts.json";
 
 #[tauri::command]
 pub async fn sync_prompts_en(proxy: Option<&str>) -> Result<(), String> {
 	println!("Sync prompts...");
-	let awesome_repo = "https://github.com/f/awesome-chatgpt-prompts/blob/main/README.md";
-	let html;
+	let awesome_repo =
+		"https://raw.githubusercontent.com/f/awesome-chatgpt-prompts/main/prompts.csv";
+	let response;
 	if let Some(proxy) = proxy {
 		let proxy = ureq::Proxy::new(proxy).unwrap();
 		let agent = ureq::AgentBuilder::new().proxy(proxy).build();
-		html = agent
+		response = agent
 			.get(awesome_repo)
 			.call()
 			.map_err(|e| e.to_string())?
 			.into_string()
 			.map_err(|e| e.to_string())?;
 	} else {
-		html = ureq::get(awesome_repo)
+		response = ureq::get(awesome_repo)
 			.call()
 			.map_err(|e| e.to_string())?
 			.into_string()
 			.map_err(|e| e.to_string())?;
 	}
+	let mut rdr = csv::Reader::from_reader(response.as_bytes());
+	let mut map = HashMap::<String, (String, i32)>::new();
+	let score = 300_usize;
+	for (idx, result) in rdr.records().enumerate() {
+		let record = result.unwrap();
+		let title = record.get(0).unwrap_or("").to_lowercase().to_string();
+		let content = record.get(1).unwrap_or("").to_string();
+		map.insert(title, (content, (score - idx) as i32));
+	}
 
-	let dom = Document::from(html.as_str());
-	let mut map: HashMap<String, (String, i32)> = HashMap::new();
-	let mut titles = vec![];
-	let mut contents = vec![];
-	for title_node in dom.find(Name("h2")) {
-		let mut title = title_node.text().to_lowercase();
-		if !title.contains("act as") {
-			continue;
-		}
-		title =
-			title.replace("act as", "").replace(" an ", "").replace(" a ", "").trim().to_string();
-		titles.push(title);
-	}
-	for blockquote in dom.find(Name("blockquote")) {
-		let content = blockquote.text().trim().to_string();
-		let check_txt = blockquote.prev().unwrap().prev().unwrap().text().to_lowercase();
-		let check_txt2 = blockquote
-			.prev()
-			.unwrap()
-			.prev()
-			.unwrap()
-			.prev()
-			.unwrap()
-			.prev()
-			.unwrap()
-			.text()
-			.to_lowercase();
-		if check_txt.contains("contributed")
-			|| check_txt.contains("act")
-			|| check_txt2.contains("contributed")
-			|| check_txt2.contains("act")
-		{
-			contents.push(content);
-		}
-	}
-	for i in 0..titles.len() {
-		let content_idx = i + contents.len() - titles.len();
-		map.insert(titles[i].clone(), (contents[content_idx].clone(), 0));
-	}
 	write_file(PROMPTS_FILE, serde_json::to_string(&map).unwrap());
-	println!("Sync prompts done. {}, {}", titles.len(), contents.len());
+	println!("Sync prompts done.");
 	Ok(())
 }
 
